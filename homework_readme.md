bc9d6f8da27306cad39e4058de0b1719b8cd41ae
shibegora <shibegora@gmail.com>
2025-01-24 15:17:40 +0300

    конечный апдейт

---
+++ homework_readme.md
@@ -0,0 +1,48 @@
+Задание 1
+
+Пункт 2
+
+![alt text](image.png)
+
+
+Задание 3
+
+![alt text](<Снимок экрана 2025-01-24 в 01.04.58.png>) 
+![alt text](<Снимок экрана 2025-01-24 в 01.06.06.png>) 
+![alt text](<Снимок экрана 2025-01-24 в 01.05.20.png>)
+![alt text](image-1.png)
+
+Задание 4
+
+Пункт 1
+ВМ создана
+![alt text](image-2.png)
+
+Пункт 2
+![alt text](image-3.png)
+
+Пункт 3
+
+```
+#! /bin/bash
+
+REPO_PATH=/opt/shvirtd-example-python
+
+# get repo
+git clone https://github.com/shibegora/shvirtd-example-python.git $REPO_PATH
+
+# run project
+cd $REPO_PATH
+docker compose up -d
+cd
+```
+Пункт 4
+![alt text](<Снимок экрана 2025-01-24 в 13.29.05.png>)
+
+Пункт 6 
+![alt text](<Снимок экрана 2025-01-24 в 13.30.01.png>)
+
+Задание 6
+
+![alt text](<Снимок экрана 2025-01-24 в 13.41.06.png>) 
+![alt text](<Снимок экрана 2025-01-24 в 13.40.17.png>)
--- setup_repo.sh
+++ setup_repo.sh
@@ -1,45 +1,11 @@
-set -e
+#! /bin/bash
 
+REPO_PATH=/opt/shvirtd-example-python
 
-REPO_URL="https://github.com/shibegora/shvirtd-example-python.git"
-REPO_DIR="/opt/shvirtd-example-python"
+# get repo
+git clone https://github.com/shibegora/shvirtd-example-python.git $REPO_PATH
 
-echo "Обновление пакетов и установка зависимостей..."
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release software-properties-common
-
-echo "Добавление GPG ключа Docker..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-echo "Добавление Docker репозитория..."
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-
-echo "Установка Docker Engine..."
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-echo "Добавление текущего пользователя в группу docker..."
-sudo usermod -aG docker $USER
-
-echo "Установка Docker Compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/2.20.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-echo "Проверка установленных версий Docker и Docker Compose..."
-docker --version
-docker-compose --version
-
-echo "Клонирование репозитория в $REPO_DIR..."
-sudo git clone $REPO_URL $REPO_DIR
-
-cd $REPO_DIR
-
-echo "Запуск Docker Compose..."
-sudo docker compose -f compose.yaml -f proxy.yaml up -d --build
-
-echo "Проект успешно запущен."
-
-exit 0
+# run project
+cd $REPO_PATH
+docker compose up -d
+cd
